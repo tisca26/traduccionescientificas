d065312618d795eccd612e08ea3a2e4c5a9ed825
tisca26 <gerry.t26@gmail.com>
2017-01-20 21:00:28 -0600

    Contacto funcional
    Se genera el contacto funcional

--- application/controllers/Contacto.php
+++ application/controllers/Contacto.php
@@ -54,15 +54,15 @@ class Contacto extends CI_Controller
             )
         );
         $mail->isSMTP();
-        $mail->Host = 'kin.hosting-mexico.net';
+        $mail->Host = 'mail.traduccionescientificas.com.mx';
         $mail->SMTPAuth = true;
         $mail->SMTPDebug = 0;
         $mail->SMTPKeepAlive = true; // SMTP connection will not close after each email sent, reduces SMTP overhead
         $mail->SMTPAuth = true;
-        $mail->SMTPSecure = 'ssl';
+        $mail->SMTPSecure = '';
         $mail->Username = 'contacto@traduccionescientificas.com.mx'; // SMTP username
         $mail->Password = 'Hola123!';
-        $mail->Port = 465;
+        $mail->Port = 26;
         $mail->AltBody = 'Para mostrar el mensaje correctamente, por favor, use un visor de email compatible con HTML, ¡Gracias!';
         $mail->CharSet = 'UTF-8';
         $mail->ContentType = 'text/html; charset=utf-8\r\n';
@@ -99,15 +99,15 @@ class Contacto extends CI_Controller
             )
         );
         $mail->isSMTP();
-        $mail->Host = 'kin.hosting-mexico.net';
+        $mail->Host = 'mail.traduccionescientificas.com.mx';
         $mail->SMTPAuth = true;
         $mail->SMTPDebug = 0;
         $mail->SMTPKeepAlive = true; // SMTP connection will not close after each email sent, reduces SMTP overhead
         $mail->SMTPAuth = true;
-        $mail->SMTPSecure = 'ssl';
+        $mail->SMTPSecure = '';
         $mail->Username = 'contacto@traduccionescientificas.com.mx'; // SMTP username
         $mail->Password = 'Hola123!';
-        $mail->Port = 465;
+        $mail->Port = 26;
         $mail->AltBody = 'Para mostrar el mensaje correctamente, por favor, use un visor de email compatible con HTML, ¡Gracias!';
         $mail->CharSet = 'UTF-8';
         $mail->ContentType = 'text/html; charset=utf-8\r\n';
